dd8c0131b334dfab9e5657e19cdb50301bafe80a
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-02-21 15:25:44 -0300

    Aula 1

---
+++ src/services/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { Database } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth();
+export const database = getAuth();
